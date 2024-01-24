91a78376f0490adcb3570ae8bce232e11f164d93
이원찬 <dnjscks4257@naver.com>
2024-01-24 18:01:15 +0900

    추가, 삭제 로직 확인해야함

--- lib/src/controller/cardController.dart
+++ lib/src/controller/cardController.dart
@@ -14,6 +14,26 @@ class CardController extends GetxController {
 
   List<CardModel> list = [];
 
+  // list delete
+  void deleteCard(CardModel model) async {
+    list.removeWhere((element) => element.cardId == model.cardId);
+    update();
+  }
+
+  // list clear
+  void clearCard() {
+    list.clear();
+  }
+
+  // 내가 추가한 모든 명함 조회
+  getAddingCardList() async {
+    List jsonData = await cardConnect.getAddingCardList();
+
+    List<CardModel> tmp = jsonData.map((m) => CardModel.parse(m)).toList();
+    list.addAll(tmp);
+    update();
+  }
+
   //명함 조회
   getMyCardList() async {
     var name = await cardConnect.getMyCardList();
@@ -44,9 +64,9 @@ class CardController extends GetxController {
 
   // 내 지갑에 명함 추가
   Future<bool> addCard(int cardId) async {
+
     try {
       await cardConnect.addCard(cardId);
-      // await feedIndex();
       return true;
     } catch (e) {
       ScaffoldMessenger.of(Get.context!).showSnackBar(SnackBar(
@@ -58,8 +78,10 @@ class CardController extends GetxController {
 
   // 내 지갑에서 명함 삭제
 
+
   //명함 지갑 조회
 
+  // 명함 등록
   Future<bool> cardRegister(String position, String organization,
       String address, int tell, String email, dynamic photo) async {
     try {
--- lib/src/widget/cardListItem.dart
+++ lib/src/widget/cardListItem.dart
@@ -11,6 +11,9 @@ class CardListItem extends StatefulWidget {
 }
 
 class _CardListItemState extends State<CardListItem> {
+
+  final String defaultImage = "https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F7eaf2b61-0388-45f2-971d-66d3149b1794%2F6d49ebcf-b41b-4c37-8275-1af8980f7b91%2FUntitled.png?table=block&id=d8c68308-ce63-487c-93c6-bb267bbaddfc&spaceId=7eaf2b61-0388-45f2-971d-66d3149b1794&width=2000&userId=2d84c7f9-2777-4bd6-aa0b-c963f16a6468&cache=v2";
+
   void _detailSubmitForm() async {
     //검색 결과 명함 상세을 누를때 로직
     Navigator.pushNamed(context, '/detail');
@@ -33,7 +36,7 @@ class _CardListItemState extends State<CardListItem> {
           // 사진
           ClipRRect(
             borderRadius: BorderRadius.circular(8),
-            child: Image.network("${widget.model.photo}",
+            child: Image.network(widget.model.photo == null ? defaultImage : "${widget.model.photo}",
                 width: 120,
                 height: 190, errorBuilder: (context, error, stackTrace) {
               return Image.asset('asset/logo.png', width: 120, height: 180);
